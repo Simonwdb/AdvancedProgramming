fa82776b2a0f963918d93a4c85aafa373011d644
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2020-09-30 18:00:10 +0200

    Add files via upload

--- assignment2/LinkedList.java
+++ assignment2/LinkedList.java
@@ -1,143 +1,153 @@
 package nl.vu.labs.phoenix.ap;
 
+import java.io.PrintStream;
 
 public class LinkedList<E extends Comparable<E>> implements ListInterface<E> {
-	
-	Node list;
-	
+
+	Node current;
+	PrintStream out;
+
 	public LinkedList() {
-		list = null;
+		current = null;
+		out = new PrintStream(System.out);
 	}
-	
-	private class Node {
 
-        E data;
-        Node prior, next;
+	// change back to private!!
+	public class Node {
 
-        public Node(E data) {
-            this(data, null, null);
-        }
+		E data;
+		Node prior, next;
 
-        public Node(E data, Node prior, Node next) {
-            this.data = data == null ? null : data;
-            this.prior = prior;
-            this.next = next;
-        }
-    }
-	
-    @Override
-    public boolean isEmpty() {
-        return list == null;
-    }
+		public Node(E data) {
+			this(data, null, null);
+		}
 
-    @Override
-    public ListInterface<E> init() {
-    	list = null;
-        return this;
-    }
+		public Node(E data, Node prior, Node next) {
+			this.data = data == null ? null : data;
+			this.prior = prior;
+			this.next = next;
+		}
+	}
 
-    @Override
-    public int size() {
-        return 0;
-    }
+	@Override
+	public boolean isEmpty() {
+		return current == null;
+	}
 
-    @Override
-    public ListInterface<E> insert(E d) {
-    	// insert d to the empty list
-    	if (isEmpty()) {
-    		list = new Node(d);
-    	}
-    	
-    	// insert d after the first element
-    	
-//    	if (list.next == null) {
-//    		list.next = new Node(d, list, null);
-//    	}
-    	
-    	if (goToNext()) {
-    		list.next = new Node(d, list, null);
-    	}
-    	
-    	// insert d at the last element
-    	
-    	/*while(list.next != null) {
-    		list = list.next;
-    	}
-    	list.next = new Node(d, list, null);
-    	*/
-    	
-    	if (goToLast()) {
-    		list.next = new Node(d, list, null);
-    	}
-    	
-    	// insert d "in the middle"
-    	
-        return this;
-    }
+	@Override
+	public ListInterface<E> init() {
+		current = null;
+		return this;
+	}
 
-    @Override
-    public E retrieve() {
-    	if (isEmpty()) {
-    		return null;
-    	}
-        return list.data;
-    }
+	@Override
+	public int size() {
+		// if tested manually there are no problems, but with the JUnit test problems
+		// occur.
+		if (isEmpty()) {
+			return 0;
+		} else if (current.next == null) {
+			return 1;
+		}
+		goToNext();
+		return 1 + size();
+	}
 
-    @Override
-    public ListInterface<E> remove() {
-        return this;
-    }
+	@Override
+	public ListInterface<E> insert(E d) {
+		// Do we need to sort the inserted d in this method or is that already been
+		// done?
+		// Need to be done over, size can't be calculated.
+		Node n = new Node(d);
+		n.next = null;
+		n.prior = current;
 
-    @Override
-    public boolean find(E d) {
-    	if (isEmpty()) {
-    		return false;
-    	}
-        return list.data == d || find(list.next.data);
-    }
+		if (current != null) {
+			current.next = n;
+		}
+		current = n;
+		return this;
+	}
 
-    @Override
-    public boolean goToFirst() {
-    	if (isEmpty()) {
-    		return false;
-    	}
-    	while (list.prior != null) {
-    		list.prior = list;
-    	}
-        return true;
-    }
+	@Override
+	public E retrieve() {
+		if (isEmpty()) {
+			return null;
+		}
+		return current.data;
+	}
 
-    @Override
-    public boolean goToLast() {
-    	if (isEmpty()) {
-    		return false;
-    	}
-    	while (list.next != null) {
-    		list.next = list;
-    	}
-        return true;
-    }
+	@Override
+	public ListInterface<E> remove() {
+		if (isEmpty()) {
+			return null;
+		} else if (current.prior == null && current.next == null) {
+			out.println("Komt binnen");
+			current = null;
+		} else if (current.next == null) {
+			current.prior.next = current.next; // do we need to say this or can be said = null?
+			goToPrevious();
+		} else if (current.prior == null) {
+			current.next.prior = current.prior; // same as above
+			goToNext();
+		} else {
+			current.prior.next = current.next;
+			current.next.prior = current.prior;
+			goToNext();
+		}
+		return this;
+	}
 
-    @Override
-    public boolean goToNext() {
-    	if (isEmpty() || list.next == null) {
-    		return false;
-    	}
-    	list = list.next;
-        return true;
-    }
+	@Override
+	public boolean find(E d) {
+		if (isEmpty()) {
+			return false;
+		}
+		return current.data == d || find(current.next.data);
+	}
 
-    @Override
-    public boolean goToPrevious() {
-    	if (isEmpty() || list.prior == null) {
-    		return false;
-    	}
-    	list = list.prior;
-        return true;
-    }
+	@Override
+	public boolean goToFirst() {
+		if (isEmpty()) {
+			return false;
+		}
+		while (current.prior != null) {
+			current = current.prior;
+		}
+		return true;
+	}
 
-    @Override
-    public ListInterface<E> copy() {
-        return this;
-    }
+	@Override
+	public boolean goToLast() {
+		if (isEmpty()) {
+			return false;
+		}
+		while (current.next != null) {
+			current = current.next;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean goToNext() {
+		if (isEmpty() || current.next == null) {
+			return false;
+		}
+		current = current.next;
+		return true;
+	}
+
+	@Override
+	public boolean goToPrevious() {
+		if (isEmpty() || current.prior == null) {
+			return false;
+		}
+		current = current.prior;
+		return true;
+	}
+
+	@Override
+	public ListInterface<E> copy() {
+		return this;
+	}
 }
