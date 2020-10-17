6b24910a691f72d6e7682a30c0fa13850164753f
lucask-code <71138732+lucask-code@users.noreply.github.com>
2020-10-17 16:14:30 +0200

    Add files via upload

--- assignment2/Main.java
+++ assignment2/Main.java
@@ -5,37 +5,37 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main<T> {
-	
+
 	private PrintStream out;
-	
+
 	Main() {
 		out = new PrintStream(System.out);
 	}
-	
-	private void print(T obj) {
-		// example a = {1, 2}
+
+	private void print(SetInterface<BigInteger> result) {
+		result = result.copy();
 		out.println("Print statement");
-		out.println(obj); 	// will print something like nl.vu.labs.phoenix.ap.Set@......
-		// desired outcome is: 1, 2		(don't know how to get to the elements of obj)
+		while(!result.isEmpty()) {
+			BigInteger temp = result.get();
+			out.print(temp.toString() + " ");
+			result.remove(temp);
+		}
 	}
-	
+
 	private void start() {
 		InterpreterInterface<Set<BigInteger>> interpreter = new Interpreter<Set<BigInteger>>();
 		Scanner in = new Scanner(System.in);
-		while(in.hasNext()) {
-			T temp = (T) interpreter.eval(in.nextLine());
-			if ( temp != null) {
-				// need to be printing
-				print(temp);
-			};
+		while (in.hasNext()) {
+			SetInterface<BigInteger> result = interpreter.eval(in.nextLine());
+			if (result != null) {
+				print(result);
+			}
+			;
 		}
-		
-		
-		//1. Create a scanner on System.in
-		//2. call interpreter.eval() on each line
-		
+
+
 	}
-	
+
 	public static void main(String[] args) {
 		new Main().start();
 	}
